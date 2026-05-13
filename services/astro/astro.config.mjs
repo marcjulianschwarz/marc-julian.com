c5ec48051670c12461c277b94ec19aa1a0f9e741
marcjulianschwarz <info@marc-julian.de>
2026-05-13 20:04:59 +0200

    add slugs to headings

--- services/astro/astro.config.mjs
+++ services/astro/astro.config.mjs
@@ -5,12 +5,14 @@ import { fileURLToPath } from 'url';
 import { blogImages } from './src/integrations/blog-images.ts';
 import { blogConfig } from './blog.config.ts';
 import rehypeFigure from 'rehype-figure';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [blogImages(blogConfig.imagesPath)],
   markdown: {
-    rehypePlugins: [rehypeFigure],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }], rehypeFigure],
     shikiConfig: {
       themes: {
         light: 'ayu-light',
